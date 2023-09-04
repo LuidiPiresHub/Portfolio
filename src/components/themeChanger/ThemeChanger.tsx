ccf9c51cb719f8ba48982397d9cdfd5cb4a5698e
LuidiPires <luidihot@gmail.com>
2023-09-04 16:48:40 -0300

    feat: Troca de temas e save no localstorage

--- src/components/themeChanger/ThemeChanger.tsx
+++ src/components/themeChanger/ThemeChanger.tsx
@@ -5,10 +5,22 @@ import { useEffect, useState } from 'react';
 export default function ThemeChanger() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const handleTheme = (checked: boolean) => {
+    localStorage.setItem('darkMode', JSON.stringify(checked));
+    setIsDarkMode(checked);
+  }
+
   useEffect(() => {
     document.documentElement.style.setProperty('--bg', isDarkMode ? 'rgb(33, 33, 33)' : 'rgb(240, 240, 240)');
     document.documentElement.style.setProperty('--cl', isDarkMode ? 'rgb(240, 240, 240)' : 'rgb(33, 33, 33)');
   }, [isDarkMode])
+
+  useEffect(() => {
+    setIsDarkMode(JSON.parse(localStorage.getItem('darkMode')!));
+    return () => {
+      localStorage.clear();
+    }
+  }, [])
 
   return (
     <label htmlFor='themeChanger' className={styles.themeChanger}>
@@ -16,7 +28,8 @@ export default function ThemeChanger() {
         id='themeChanger'
         type='checkbox'
         className={styles.checkbox}
-        onChange={({ target: { checked }}) => setIsDarkMode(checked)}
+        onChange={({ target: { checked }}) => handleTheme(checked)}
+        checked={isDarkMode}
       />
       <div className={styles.ball} />
       <FaMoon size={20} color='#f1c30f' />
