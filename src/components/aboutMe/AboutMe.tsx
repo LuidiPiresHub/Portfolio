789a8fa10f92254d710dfae0b3df4059b8b0766a
LuidiPires <luidihot@gmail.com>
2023-09-19 19:06:51 -0300

    feat: Adiciona certificados

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -3,6 +3,7 @@ import myPhoto from '../../assets/photos/myPhoto.jpg';
 import styles from './AboutMe.module.css';
 import { FaDesktop, FaCog, FaDownload } from 'react-icons/fa';
 import cv from '../../assets/cv/LuidiPires.pdf'
+import certificates from '../../data/certificates';
 
 export default function AboutMe() {
   return (
@@ -52,6 +53,13 @@ export default function AboutMe() {
             aplicações mobile. Incluindo a criação e manipulação de bancos de dados relacionais e não-relacionais.
           </p>
         </div>
+      </section>
+      <section className={styles.certificatesContainer}>
+        {certificates.map(({ image, name }) => (
+          <a href={image} target='_blank'>
+            <img src={image} alt={name} key={name} className={styles.certificate} />
+          </a>
+        ))}
       </section>
       <a href={cv} download='Currículo Luidi Pires' className={styles.download}>
         Download CV
---
+++ src/data/certificates.ts
@@ -0,0 +1,28 @@
+import certificate1 from '../assets/certificates/certificate1.png';
+import certificate2 from '../assets/certificates/certificate2.png';
+import certificate3 from '../assets/certificates/certificate3.png';
+import certificate4 from '../assets/certificates/certificate4.png';
+import certificate5 from '../assets/certificates/certificate5.png';
+
+export default [
+  {
+    name: 'Certificado de Fundamentos',
+    image: certificate1,
+  },
+  {
+    name: 'Certificado de Frontend',
+    image: certificate2,
+  },
+  {
+    name: 'Certificado de Backend',
+    image: certificate3,
+  },
+  {
+    name: 'Certificado de Ciência da Computação',
+    image: certificate4,
+  },
+  {
+    name: 'Certificado de Desenvolvimento Web',
+    image: certificate5,
+  },
+]
