a384d368969fced592e4ee75dc6e2560dd081a13
LuidiPires <luidihot@gmail.com>
2023-09-20 01:07:18 -0300

    feat: Adiciona variaveis css para estilizar a barra lateral

--- src/components/themeChanger/ThemeChanger.tsx
+++ src/components/themeChanger/ThemeChanger.tsx
@@ -8,20 +8,22 @@ export default function ThemeChanger() {
   const handleTheme = (checked: boolean) => {
     localStorage.setItem('darkMode', JSON.stringify(checked));
     setIsDarkMode(checked);
-  }
+  };
 
   useEffect(() => {
-    document.documentElement.style.setProperty('--bgPk', isDarkMode ? 'rgb(33, 33, 33)' : 'rgb(240, 240, 240)');
-    document.documentElement.style.setProperty('--bgSc', isDarkMode ? 'rgb(50, 50, 50)' : 'rgb(255, 255, 255)');
-    document.documentElement.style.setProperty('--cl', isDarkMode ? 'rgb(240, 240, 240)' : 'rgb(33, 33, 33)');
-  }, [isDarkMode])
+    document.documentElement.style.setProperty('--bgPk', isDarkMode ? 'rgb(33 33 33)' : 'rgb(240 240 240)');
+    document.documentElement.style.setProperty('--bgSc', isDarkMode ? 'rgb(50 50 50)' : 'rgb(255 255 255)');
+    document.documentElement.style.setProperty('--cl', isDarkMode ? 'rgb(240 240 240)' : 'rgb(33 33 33)');
+    document.documentElement.style.setProperty('--scrollBar', isDarkMode ? 'rgb(51 51 51)' : 'rgb(245 245 245)');
+    document.documentElement.style.setProperty('--scrollBarThumb', isDarkMode ? 'rgb(102 102 102)' : 'rgb(200 200 200)');
+  }, [isDarkMode]);
 
   useEffect(() => {
     setIsDarkMode(JSON.parse(localStorage.getItem('darkMode')!));
     return () => {
       localStorage.clear();
-    }
-  }, [])
+    };
+  }, []);
 
   return (
     <label htmlFor='themeChanger' className={styles.themeChanger}>
@@ -36,5 +38,5 @@ export default function ThemeChanger() {
       <FaMoon size={20} color='#f1c30f' />
       <FaSun size={20} color='#f1c30f' />
     </label>
-  )
+  );
 }
